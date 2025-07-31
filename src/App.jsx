4c549ab00c1f28d67fc1593febcbf16b8b766dfc
tarun5858 <ts460000@gmail.com>
2025-07-31 16:48:37 +0530

    work done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Header from './components/Header';
 import SellerPage from "./screens/SellerPage";
 import { Routes, Route } from "react-router-dom";
+import WhatsAppButton from "./components/whatsappButton";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <WhatsAppButton></WhatsAppButton>
       <Routes>
         <Route path="/" element={<SellerPage />} />
       </Routes>
